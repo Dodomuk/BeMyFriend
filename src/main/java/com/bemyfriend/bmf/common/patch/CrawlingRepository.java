2c7fd5e62ffe69df9b0d5ae00d16c5ea430fe6de
myungjae6 <myungjae6@naver.com>
2021-04-01 15:15:09 +0900

    [myungjae/210401] 수정

---
+++ src/main/java/com/bemyfriend/bmf/common/patch/CrawlingRepository.java
@@ -0,0 +1,13 @@
+package com.bemyfriend.bmf.common.patch;
+
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CrawlingRepository {
+	@Insert("insert into calendar(event_start_date, event_end_date, event_com_name)"
+			+ "values(CURRENT_TIMESTAMP, #{event_end_date}, #{event_com_name})")
+	int insertRecruit(Map<String,String> commandMap);
+}

6768de1ede5ed0fadc1b435725e35e461625c006
dodomuk <siderid@Naver.com>
2021-04-24 03:56:56 +0900

    [210424동묵]채용공고 작성/ 수정 추가

--- src/main/java/com/bemyfriend/bmf/recruitment/model/mapper/RecruMapper.java
+++ src/main/java/com/bemyfriend/bmf/recruitment/model/mapper/RecruMapper.java
@@ -1,42 +1,41 @@
 package com.bemyfriend.bmf.recruitment.model.mapper;
 
 import java.util.List;
+import java.util.Map;
 
-import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import com.bemyfriend.bmf.common.util.file.FileVo;
 import com.bemyfriend.bmf.common.util.paging.Paging;
-import com.bemyfriend.bmf.community.model_review.vo.Review;
 import com.bemyfriend.bmf.recruitment.model.vo.Recruitment;
 
 @Mapper
 public interface RecruMapper {
 
-	public List<Recruitment> selectRecruList(Paging page); 
-	
-	public Recruitment viewRecruId(int jobNo);
-	
+	public List<Recruitment> selectRecruList(Paging page);
+
+	public Recruitment viewRecruId(String view);
+
 	public Integer insertRecru(Recruitment recruitment);
-	
+
 	public boolean deleteRecru(int no);
 	
-	//public void updateRecru(Recruitment recruitment);
-	
-	//public void pageViewCount(int no);
-	
-	// 게시글 업로드
-	
-	  //더미데이터 집어넣으려고 임시로 만듦
-	  @Insert("insert into com_hire(job_no,com_id,job_title,job_deadline,job_step,job_type,job_per_num,job_duty,job_location,job_career,job_edu,job_need,job_preper,job_welfare,job_salary,job_image)" 
-	  + " values(sc_job_idx.nextval,#{comId},#{jobTitle},#{jobDeadline},#{jobStep},#{jobType},#{jobPerNum},#{jobDuty},#{jobLocation},#{jobCareer},#{jobEdu},#{jobNeed},#{jobPreper},#{jobWelfare},#{jobSalary},#{jobImage})") 
-	  int insertRecruDummi(Recruitment recruitment);
+	public int insertRecruFile(FileVo file) throws Exception;
 
-	  @Select("select count(*) from com_hire")
-	  int selectContentCnt();
-	  
-	  @Select("select * from com_hire")
-	  Recruitment selectRecru();
-	  
+	public void updateRecru(Recruitment recruitment);
+	
+	@Delete("delete from b_file where type_idx = #{typeIdx}")
+	public boolean deleteRecruFile(String typeIdx);
    
+	// public void pageViewCount(int no);
+
+	@Select("select count(*) from com_hire")
+	int selectContentCnt();
+
+	@Select("select * from com_hire")
+	Recruitment selectRecru();
+  
+	public List<Map<String,FileVo>> selectFileWithBIdx(String view);
 }
